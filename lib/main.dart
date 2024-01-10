5355751485d8fa5f82565b0618830a69cacc9557
Ipan Suryadi <ipansuryadi@gmail.com>
2024-01-10 15:49:02 +0700

    fix: refactor again

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,43 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:simplest/setup.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'my_app.dart';
 
 void main() async {
   runApp(const MyApp());
-  await permission();
-  await firebaseSetup();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await _permission();
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  if (kDebugMode) {
+    print("ipansuryadi $fcmToken");
+  }
+
+  await FirebaseMessaging.instance.subscribeToTopic("ngetest");
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+}
+
+Future<void> _permission() async {
+  await FirebaseMessaging.instance.requestPermission(provisional: true);
+
+  // For apple platforms, ensure the APNS token is available before making any FCM plugin API calls
+  final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+  if (apnsToken != null) {
+    // APNS token is available, make FCM plugin API requests...
+  }
+  FirebaseMessaging.instance.onTokenRefresh
+      .listen((fcmToken) {})
+      .onError((err) {});
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  if (kDebugMode) {
+    print(message.data.toString());
+  }
 }
